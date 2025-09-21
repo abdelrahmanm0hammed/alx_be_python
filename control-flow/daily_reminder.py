c9a753ec8bf05980e916a78e5ae11608d5739fdd
AbdelrahmanMohamed <abdelrahmanmuhammed04@gmail.com>
2025-09-22 00:41:44 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,26 @@
 variable = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
-time_bounded = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority :
     case "high":
-        if time_bounded == "yes":
+        if time_bound == "yes":
             print("Reminder: ", variable," is a ", priority," task that requires immediate attention today!")
-        elif time_bounded == "no":
+        elif time_bound == "no":
             print("Note: ", variable," is a ", priority," task Consider completing it when you have free time.")
     case "medium":
-        if time_bounded == "yes":
+        if time_bound == "yes":
             print("Reminder: ", variable," is a ", priority," task that requires immediate attention today!")
-        elif time_bounded == "no":
+        elif time_bound == "no":
             print("Note: ", variable," is a ", priority," task Consider completing it when you have free time.")
     case "medium":
-        if time_bounded == "yes":
+        if time_bound == "yes":
             print("Reminder: ", variable," is a ", priority," task that requires immediate attention today!")
-        elif time_bounded == "no":
+        elif time_bound == "no":
             print("Note : ", variable," is a ", priority," task. Consider completing it when you have free time.")
     case "low":
-        if time_bounded == "yes":
+        if time_bound == "yes":
             print("Reminder: ", variable," is a ", priority," task that requires immediate attention today!")
-        elif time_bounded == "no":
+        elif time_bound == "no":
             print("Note : ", variable," is a ", priority," task. Consider completing it when you have free time.")
+
