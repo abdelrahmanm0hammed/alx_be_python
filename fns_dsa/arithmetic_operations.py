7e3cad5d489b7e58c1e2db01a0af719815dceccf
AbdelrahmanMohamed <abdelrahmanmuhammed04@gmail.com>
2025-09-28 19:39:30 +0300

    Create arithmetic_operations.py

---
+++ fns_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+ef perform_operation(num1, num2, operation):
+    if operation == "add" :
+        result = num1 + num2
+        return result
+    elif operation == "subtract" :
+        result = num1 - num2 
+        return result
+    elif operation == "multiply" :
+        result = num1 * num2
+        return result
+    elif operation == "divide" :
+        if num2 == 0:
+            print("Error cannot divide by zero ")
+        else : 
+            result = num1/num2
+            return result
